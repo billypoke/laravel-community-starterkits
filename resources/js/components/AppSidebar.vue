130bb634e1917ac7007718dd3f89b9498cd13ead
Ransom Pate <ransom.pate@gmail.com>
2025-03-16 09:54:39 -0400

    there are no footer nav items, fixes console warning

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -1,55 +1,47 @@
 <script setup lang="ts">
-import NavFooter from '@/components/NavFooter.vue';
 import NavMain from '@/components/NavMain.vue';
-import NavUser from '@/components/NavUser.vue';
-import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
+import { Sidebar, SidebarContent, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder, LayoutGrid, Plus, Home, Bookmark } from 'lucide-vue-next';
+import { Home, LayoutGrid, Plus } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
-    {
-        title: 'Dashboard',
-        href: '/app/dashboard',
-        icon: Home,
-
-    },
-    {
-        title: 'All Starterkits',
-        href: '/app/starterkits',
-        icon: LayoutGrid,
-    },
-    {
-        title: 'Add Starterkit',
-        href: '/app/starterkit/create',
-        icon: Plus,
-    },
+  {
+    title: 'Dashboard',
+    href: '/app/dashboard',
+    icon: Home,
+  },
+  {
+    title: 'All Starterkits',
+    href: '/app/starterkits',
+    icon: LayoutGrid,
+  },
+  {
+    title: 'Add Starterkit',
+    href: '/app/starterkit/create',
+    icon: Plus,
+  },
 ];
 </script>
 
 <template>
-    <Sidebar collapsible="icon" variant="inset">
-        <SidebarHeader>
-            <SidebarMenu>
-                <SidebarMenuItem>
-                    <SidebarMenuButton size="lg" as-child>
-                        <Link :href="route('dashboard')">
-                            <AppLogo />
-                        </Link>
-                    </SidebarMenuButton>
-                </SidebarMenuItem>
-            </SidebarMenu>
-        </SidebarHeader>
+  <Sidebar collapsible="icon" variant="inset">
+    <SidebarHeader>
+      <SidebarMenu>
+        <SidebarMenuItem>
+          <SidebarMenuButton size="lg" as-child>
+            <Link :href="route('dashboard')">
+              <AppLogo />
+            </Link>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+      </SidebarMenu>
+    </SidebarHeader>
 
-        <SidebarContent>
-            <NavMain :items="mainNavItems" />
-        </SidebarContent>
-
-        <SidebarFooter>
-            <NavFooter :items="footerNavItems" />
-            <NavUser />
-        </SidebarFooter>
-    </Sidebar>
-    <slot />
+    <SidebarContent>
+      <NavMain :items="mainNavItems" />
+    </SidebarContent>
+  </Sidebar>
+  <slot />
 </template>
